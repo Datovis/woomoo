8f2be470590de71366774c5a0a25ffca8f955809
Tommy Chen <tommy351@gmail.com>
2014-02-20 12:44:05 +0800

    Add everyone's name to `author_avatar` helper.

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,5 +1,18 @@
 var authors = {
-  'LeiLei Wu': 'lei'
+  'Ben Lin': 'ben',
+  'Shao Lee': 'shao',
+  'Leo Lin': 'leo',
+  'Jonathan Dang': 'jonathan',
+  'Carlos Liu': 'carlos',
+  'Howard Huang': 'howard',
+  'Ray Shih': 'ray',
+  'Ting-Yu Lin': '200',
+  'Rick Chen': 'rick',
+  'Lei Lei Wu': 'lei',
+  'Jason Peng': 'jason',
+  'Tommy Chen': 'tommy',
+  'TT': 'tt',
+  'JJ': 'jj'
 };
 
 hexo.extend.helper.register('author_avatar', function(name){
