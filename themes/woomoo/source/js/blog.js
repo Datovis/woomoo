1773d091a446fcf0993a493b9ff6bd4fc1e1a84c
ben <ben@dreamerslab.com>
2014-02-25 18:40:09 +0800

    move fancybox code from blog.js to after-footer view

--- themes/woomoo/source/js/blog.js
+++ themes/woomoo/source/js/blog.js
@@ -1,25 +1,4 @@
 (function($){
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
-      if ($(this).parent().hasClass('fancybox')) return;
-
-      var alt = this.alt;
-
-      if (alt) $(this).after('<span class="caption">' + alt + '</span>');
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
-    });
-
-    $(this).find('.fancybox').each(function(){
-      $(this).attr('rel', 'article' + i);
-    });
-  });
-
-  if ($.fancybox){
-    $('.fancybox').fancybox();
-  }
-
   $('#page-top').on('click', function(){
     $('html, body').animate({
       scrollTop: 0
